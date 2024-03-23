60f1c420bb74a3c7e398623f75c377509f667c24
sidd1607 <siddheshnaik.1607@gmail.com>
2024-03-23 14:43:43 +0530

    contactformupdate

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,29 +1,60 @@
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 const ContactUs = () => {
+  const [state, handleSubmit] = useForm("xkndvawz");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
   return (
     <div className="ContactUs">
       <main>
         <h1>Contact Us</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            <label>Name</label>
-            <input type="text" required placeholder="Your name" />
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              required
+              placeholder="Your name"
+              id="name"
+              name="name"
+            />
+            <ValidationError prefix="name" field="name" errors={state.errors} />
           </div>
           <div>
-            <label>Email</label>
-            <input type="email" required placeholder="abc@xyz.com" />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              required
+              placeholder="abc@xyz.com"
+              id="email"
+              name="email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
           <div>
-            <label>Message</label>
+            <label htmlFor="message">Message</label>
             <input
               type="text"
               required
               placeholder="Tell us about your Query..."
+              id="message"
+              name="message"
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
             />
           </div>
-
-          <button type="submit">Send</button>
+          <button type="submit" disabled={state.submitting}>
+            Send
+          </button>
         </form>
       </main>
     </div>
